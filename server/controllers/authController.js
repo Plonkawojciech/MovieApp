d612bffcdb043abc397078118296b46a6ce49d56
Wojtek <plonkawojtek06@gmail.com>
2025-06-07 19:23:35 +0200

    adding to the database from login form

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,28 +1,37 @@
-exports.handleLogin = (req, res) => {
-  console.log(req.body)
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+console.log("test");
 
-  const errors = []
 
-  if (typeof req.body.username !== "string") req.body.username = ""
-  if (typeof req.body.password !== "string") req.body.password = ""
+exports.handleLogin = async (req, res) => {
+  const errors = [];
 
-  req.body.username = req.body.username.trim()
+  if (typeof req.body.username !== "string") req.body.username = "";
+  if (typeof req.body.password !== "string") req.body.password = "";
 
-  if (!req.body.username) errors.push('You must provide a username.')
-  if (req.body.username && req.body.username.length < 3) errors.push('Username must be at least 3 characters.')
-  if (req.body.username && req.body.username.length > 10) errors.push('Username cannot exeed 10 characters.')
-  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain letters and numbers");
-  
-  if (!req.body.password) errors.push('You must provide a Password.')
-  if (req.body.password && req.body.password.length < 8) errors.push('Password must be at least 8 characters.')
-  if (req.body.password && req.body.password.length > 50) errors.push('Password cannot exeed 50 characters.')
-  
+  req.body.username = req.body.username.trim();
+
+  if (!req.body.username) errors.push("You must provide a username.");
+  if (req.body.username && req.body.username.length < 3)
+    errors.push("Username must be at least 3 characters.");
+  if (req.body.username && req.body.username.length > 10)
+    errors.push("Username cannot exeed 10 characters.");
+  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))
+    errors.push("Username can only contain letters and numbers");
+
+  if (!req.body.password) errors.push("You must provide a Password.");
+  if (req.body.password && req.body.password.length < 8)
+    errors.push("Password must be at least 8 characters.");
+  if (req.body.password && req.body.password.length > 50)
+    errors.push("Password cannot exeed 50 characters.");
+
   if (errors.length) {
-    return res.status(400).json({ errors })
+    return res.status(400).json({ errors });
   }
-  //save the new user into a database
+  console.log("Tworzenie użytkownika...");
+  const user = await prisma.user.create({ data: { name: req.body.username, password: req.body.password } });
+  console.log("Utworzono:", user);
 
-  //log the user in then give them a cookie
+  res.send("Thank you for filling out the form");
+};
 
-  res.send('Thank you for filling out the form')
-}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require("cors");
-const db = require('better-sqlite3')("ourApp.db")
-db.pragma('journal_mode = WAL')
 const authRoutes = require("./routes/auth");
 const app = express();
 const PORT = 5000;
+
+app.use((req, res, next) => {
+  console.log(`🔥 ${req.method} ${req.url}`);
+  next();
+});
+
 
 app.use(cors())
 app.use(express.json());
